741b90575c6c65ab93cb4d3c90150bc06a0e30fe
07tuma <98833473+07tuma@users.noreply.github.com>
2022-03-30 15:51:58 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+let buttonKnock = document.querySelector(".button-knock");
+let buttonOpen = document.querySelector(".button-open");
+let doorClose = document.querySelector(".door-close");
+let doorOpen = document.querySelector(".door-open");
+let doorCloseClick = document.querySelector(".door-close-click");
+
+
+
+buttonOpen.addEventListener("click", function () {
+	doorCloseClick.style.opacity = "0";
+	setTimeout(returnOrigOpen, 1000);
+	
+})
+
+buttonKnock.addEventListener("click", function () {
+	setTimeout(returnOrigClose, 1000);
+	doorClose.style.opacity = "1";
+	doorOpen.style.opacity = "0";
+	doorClose.style.width = "100%";
+	doorClose.style.height = "55%";
+	console.log("11111111");
+})
+
+function returnOrigClose () {
+	doorCloseClick.style.opacity = "1";
+	  }
+
+	  function returnOrigOpen () {
+		doorOpen.style.opacity = "1";
+	doorClose.style.width = "0%";
+	doorClose.style.height = "0%";
+	doorOpen.style.width = "100%";
+	doorOpen.style.height = "55%";
+	console.log("11111111");
+		  }
+  
+  
+
+
+
